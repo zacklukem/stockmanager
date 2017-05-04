ae3122b75794fe319c7d9bc3938b4e9fe3795ece
Richard Liu <richy.liu.2002@gmail.com>
2017-05-03 20:44:06 -0700

    add code from my repo

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from datetime import datetime
-from Slot import Slot
+from datetime import date
+from Stock import Stock
 from Util import getStockPrice, prettyPrint
+from Portfolio import Portfolio
 
 
+print('\n\n############################################################################################')
+# stock = Stock('AAPL')
+# stock.buy(10, date(2017, 2, 9))
+# print(stock)
+# stock.sell(3, date(2017, 3, 1))
+# print(stock)
+# stock.buy(13, date(2017, 4, 27))
+# print(stock)
 
-s = Slot('AAPL', datetime(2017, 4, 20))
-prettyPrint(['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'], s.getValue())
+account = Portfolio(1000000)
+account.buy('AAPL', 20, date(2016, 12, 6))
+account.sell('AAPL', 20, date(2017, 3, 31))
+# account.buy('GOOG', 10, date(2017, 1, 9))
+# account.buy('GOOG', 25, date(2017, 1, 21))
+# account.buy('GOOG', 17, date(2017, 4, 5))
+print(account)
 
-# prettyPrint(['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'], getStockPrice('AAPL', datetime(2017, 4, 23), datetime(2017, 4, 29)))
+print('############################################################################################')
